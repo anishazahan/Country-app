a23cedf8678c819f930e979230a09e057a7113bb
anishazahan <anishazahan13@gmail.com>
2022-08-18 14:34:08 +0600

    data fetch consolegs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+const url ='https://restcountries.com/v3.1/all';
+const [countries,setCountries] = useState([]);
+const [isLoading,setIsLoading] = useState(true);
+const [error,setError] = useState(null);
+
+const fetchData =async (url)=>{
+setIsLoading(true);
+try{
+
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+ setCountries(data);
+ setIsLoading(false);
+ setError(null);
+
+
+}catch(error){
+  setIsLoading(false);
+ setError(error);
+
+}
+
+
+}
+
+  useEffect(()=>{
+    fetchData(url);
+  },[])
+
   return (
     <div className="App">
      <h2>Search Your country</h2>
+
+
+
     </div>
   );
 }
